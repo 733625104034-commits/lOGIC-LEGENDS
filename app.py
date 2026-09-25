ac71af7fb9932f30d53a58ef64553b600d48a114
733625104034-commits <733625104034@hindusthan.net>
2026-09-25 13:17:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title("Smart Resume Analyzer & Job Matching System")
 
 st.file_uploader("Upload Your Resume", type=["pdf", "docx"])
+job_description = st.text_area("Paste Job Description")
